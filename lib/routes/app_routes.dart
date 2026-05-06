114b0d3dbf9fe259cad6220c059e7f6aeb14ede1
Jasper13 <adrianpjasper@gmail.com>
2026-05-06 18:52:50 -0300

    organizando pastas

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const login = '/';
+  static const home = '/home';
+  static const wardrobe = '/wardrobe';
+  static const addClothing = '/add-clothing';
+  static const friends = '/friends';
+  static const suggestions = '/suggestions';
+}
--- routes/app_routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const login = '/';
-  static const home = '/home';
-  static const wardrobe = '/wardrobe';
-  static const addClothing = '/add-clothing';
-  static const friends = '/friends';
-  static const suggestions = '/suggestions';
-}
